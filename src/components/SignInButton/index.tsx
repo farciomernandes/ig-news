b52dcdbc015941db9e51485c3db99f97f2344cbe
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-23 11:23:34 -0300

    projeto criado com next, usando NextAuth para autenticacao com github e integrado ao Stripe

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,34 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import { signIn, useSession, signOut } from 'next-auth/client';
+
+
+import styles from './styles.module.scss';
+
+export function SigninButton(){
+    const [session] = useSession();
+
+    console.log(session);
+
+        return session ? (
+            <button 
+                className={styles.signInButton} 
+                type="button"
+                onClick={() => signOut()}
+                >
+                <FaGithub color="#eba417" />
+                {session.user.name}
+                <FiX color="#737380" className={styles.closeIcon}/>
+            </button>
+        ) : (
+            <button 
+                className={styles.signInButton} 
+                type="button"
+                onClick={() => signIn('github')}
+                >
+                    <FaGithub color="#04d361" />
+                    Sign in with Github
+            </button>
+        )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from 'next';
+
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+/**
+ * 3 formas de buscar uma informação no next
+ * Client-side
+ * Server-side (SSR) -> Comentários de um post
+ * Static-side (SSG) -> Conteúdos de um Post
+ */
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  console.log(product)
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get acess to all publications <br/><span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async() =>{
+  const price = await stripe.prices.retrieve('price_1JGNrCIxrY1DjMmonl0h94wH', {
+    expand: ['product'] //recupera o preço e todas as informações do produto
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount / 100), /*Vem em centavos*/)
+  };
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 1 minuto, 1 hora, 24 horas
+  }
+}
+
+
