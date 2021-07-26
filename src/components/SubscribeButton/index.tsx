1250ff54b86873e179b27134a6780f5ad838c209
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-26 10:46:23 -0300

    cria cliente no stripe e utilizar a lib stripe-js para poder usar stripe no browser.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 
+import { signin, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -6,10 +9,33 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton( { priceId }: SubscribeButtonProps){
+    const [ session ] = useSession();
+
+    async function handleSubscribe(){
+        if(!session){ // se o usuario não estiver logado
+            signin('github')
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe');
+
+            const { sessionsId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId: sessionsId});
+        } catch (error) {
+            alert(error.message);
+        }
+
+        
+    }
     return(
         <button 
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
         Subscribe now
         </button>
