19f16280044218df811550414cd1ce82405a463b
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-27 13:07:25 -0300

    adiciona conexao com prismic e renderiza em tela os posts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f0b03d338f0991db4056"; }
+/******/ 		__webpack_require__.h = function() { return "10b55a5c467965a26b05"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
