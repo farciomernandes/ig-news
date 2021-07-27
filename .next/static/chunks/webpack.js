b6972eb4ed25357317098c010c4f61146386e58c
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-27 14:37:58 -0300

    app finalizado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c8978e0311dbca713ffe"; }
+/******/ 		__webpack_require__.h = function() { return "f575d70e70f7f8560402"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
