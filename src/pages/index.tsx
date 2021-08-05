deec9b37d505dee7c29a29d580004d02a5110886
Marcio Fernandes <farciomernandes@gmail.com>
2021-08-05 10:46:26 -0300

    adiciona teste a todas as paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ import styles from './home.module.scss';
 interface HomeProps {
   product: {
     priceId: string,
-    amount: number,
+    amount: string,
   }
 }
 
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
           <p>
             Get acess to all publications <br/><span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,91 +1,90 @@
 import { GetStaticPaths, GetStaticProps } from "next"
-import { useSession } from "next-auth/client";
-import { useRouter } from "next/dist/client/router";
-import Head from "next/head";
-import Link from "next/link";
-import { RichText } from "prismic-dom";
-import { useEffect } from "react";
-import { getPrismicClient } from "../../../services/prismic";
+import { getSession, useSession } from "next-auth/client"
+import Head from "next/head"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { RichText } from "prismic-dom"
+import { useEffect } from "react"
+import { getPrismicClient } from "../../../services/prismic"
 
-import styles from '../post.module.scss';
+import styles from '../post.module.scss'
 
-interface PostPreviewProps{
-    post: {
-        slug: string;
-        title: string;
-        content: string;
-        updatedAt: string;
-    }
+interface PostPreviewProps {
+  post: {
+    slug:string;
+    title:string;
+    content:string;
+    updatedAt:string;
+  }
 }
 
-export default function PostPreview({ post }: PostPreviewProps){
-    const [ session ] = useSession();
-    const router = useRouter();
+export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession()
+  const router = useRouter()
 
-    useEffect(()=>{
-        if(session?.activeSubscription){
-            router.push(`/posts/${post.slug}`)
-        }
-    },[session])
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [session])
 
-    return(
-        <>
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
 
-        <Head>
-            <title>{post.title} | Ignews</title>
-        </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
 
-        <main className={styles.container}>
-            <article className={styles.post}>
-                <h1>{post.title}</h1>
-                <time>{post.updatedAt}</time>
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
 
-                <div 
-                className={`${styles.postContent}  ${styles.previewContent}`}
-                dangerouslySetInnerHTML={{ __html: post.content }} />
-
-                <div className={styles.continueReading}>
-                    Wanna continue reading?
-                    <Link href="/">
-                        <a href="/"> Subscribe now 🤗 </a>
-                    </Link>
-                </div>
-            </article>
-        </main>
-
-        </>
-    )
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="home">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  )
 }
 
-export const getStaticPaths: GetStaticPaths = async() => {
-    return {
-        paths: [],
-        fallback: 'blocking'
-    }
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) =>{
-    const { slug } = params;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
 
-    const prismic = getPrismicClient();
+  const prismic = getPrismicClient()
 
-    const response = await prismic.getByUID('publication', String(slug), {});
+  const response = await prismic.getByUID('publication', String(slug), {})
 
-    const post = {
-        slug,
-        title: RichText.asText(response.data.title),
-        content: RichText.asHtml(response.data.content.splice(0, 3)),
-        updatedAt:new Date(response.last_publication_date).toLocaleDateString('pt-BR',{
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-        })
-    };
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
 
-    return {
-        props:{
-            post,
-        },
-        redirect: 60 * 30 // 30 minutos
-    }
+  return {
+    props: {
+      post
+    },
+    revalidate: 60 * 30, // 30 minutes
+  }
 }
